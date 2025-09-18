ee9ca94d5b357adf250b15bdb9209d1d9a75df45
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-09-18 20:39:01 -0300

    Refactor urls.py to enhance comments for better clarity on admin and API paths

--- config/urls.py
+++ config/urls.py
@@ -32,9 +32,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    # Admin
+    # Admin site
     path("admin/", admin.site.urls),
-    # JWT
+    # Django Rest Framework
+    path("api-auth/", include("rest_framework.urls")),
+    # Simple JWT
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
